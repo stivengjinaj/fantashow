fae420c25e67d2678a32d4264895b19039640185
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-07 22:43:14 +0100

    Server setup

---
+++ server/server.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import router from "./routes.mjs";
+const app = express();
+
+const corsOptions = {
+    origin: '*',
+    methods: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
