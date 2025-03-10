23c6e8a6935c758dc3026ca1fc4a2288a3c73957
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-10 12:51:37 +0100

    Implemented logout function

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,13 +2,22 @@ import { signInWithEmailAndPassword, browserSessionPersistence, browserLocalPers
 import { auth } from "./firebase.mjs";
 import getError from "./errorHandler.js";
 
-const loginWithEmail = async (email, password, rememberMe=false) => {
+export const loginWithEmail = async (email, password, rememberMe=false) => {
     try {
         await setPersistence(auth, rememberMe ? browserSessionPersistence : browserLocalPersistence);
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         return { success: true, user: userCredential.user };
     } catch (error) {
         console.log(error);
+        return { success: false, message: getError(error.code) };
+    }
+};
+
+export const logout = async () => {
+    try {
+        await auth.signOut();
+        return { success: true };
+    } catch (error) {
         return { success: false, message: getError(error.code) };
     }
 };
