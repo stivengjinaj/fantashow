8e2b964826f7f786cd99861cdb733a369f3c6656
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-10 11:22:32 +0100

    Added global user state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,25 @@ import Login from "./screens/Authentication/Login.jsx";
 import ReferralLink from "./screens/Referral/ReferralLink.jsx";
 import NotFound from "./screens/NotFound.jsx";
 import Support from "./screens/Support/Support.jsx";
+import {useEffect, useState} from "react";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "./utils/firebase.mjs";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+
+        return () => unsubscribe();
+    }, []);
 
   return (
       <Routes>
           <Route index path="/:contact?" element={<Homepage/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/login" element={<Login user={user}/>}/>
           <Route path="/referral/:referral" element={<ReferralLink/>}/>
           <Route path="/support" element={<Support/>}/>
           <Route path="*" element={<NotFound/>}/>
