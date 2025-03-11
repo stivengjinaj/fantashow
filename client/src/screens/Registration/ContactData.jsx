356533ecf9b84cb573435595a3e70c59f1e7d873
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-11 13:00:24 +0100

    Added form for contact data with validation

---
+++ client/src/screens/Registration/ContactData.jsx
@@ -0,0 +1,56 @@
+import {Button, Container, Form, Image} from "react-bootstrap";
+import {useState} from "react";
+import next from "../../assets/icons/next.svg";
+
+
+function ContactData({dispatch, state, nextStep, prevStep}) {
+    const [cellulareError, setCellulareError] = useState(false);
+
+    const handleChange = (e) => {
+        cellulareError && setCellulareError(false);
+        dispatch({type: "UPDATE_DATA", payload: {[e.target.name]: e.target.value}});
+    }
+
+    const handleNext = () => {
+        !state.cellulare ? setCellulareError(true) : nextStep();
+    }
+
+    return (
+        <Container fluid className="d-flex flex-column align-items-center justify-content-center">
+            <h2 className="text-light text-center mt-5">Dati Personali</h2>
+            <Form className="mt-5 login-form-width">
+                <Form.Group className="mb-5">
+                    <Form.Control
+                        type="number"
+                        placeholder="Cellulare*"
+                        className="outlined-orange-input"
+                        name="cellulare"
+                        value={state.cellulare || ""}
+                        onChange={handleChange}
+                    />
+                    {cellulareError && <p className="mx-2 text-danger mb-5">Il campo cellulare è obbligatorio</p>}
+                </Form.Group>
+                <Form.Group className="mb-5">
+                    <Form.Control
+                        type="text"
+                        placeholder="Telegram"
+                        className="outlined-orange-input"
+                        name="telegram"
+                        value={state.telegram || ""}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+            </Form>
+            <Container fluid className="d-flex justify-content-center mt-3">
+                <Button onClick={prevStep} className="sendIcon p-3 mt-3 mx-5">
+                    <Image src={next} width={30} height={30} style={{transform: `rotate(-180deg)`}}/>
+                </Button>
+                <Button onClick={handleNext} className="sendIcon p-3 mt-3 mx-5">
+                    <Image src={next} width={30} height={30}/>
+                </Button>
+            </Container>
+        </Container>
+    );
+}
+
+export default ContactData;
