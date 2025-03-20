aa90642295eec2140af85d6c894a5ef2f4338c00
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-20 22:03:37 +0100

    Added reset password route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import {Navigate, Route, Routes} from "react-router";
 import Homepage from "./screens/Home/Homepage.jsx";
 import Login from "./screens/Authentication/Login.jsx";
 import ReferralLink from "./screens/Referral/ReferralLink.jsx";
-import NotFound from "./screens/NotFound.jsx";
+import NotFound from "./screens/misc/NotFound.jsx";
 import Support from "./screens/Support/Support.jsx";
 import {useEffect, useState} from "react";
 import {onAuthStateChanged} from "firebase/auth";
@@ -13,6 +13,7 @@ import Profile from "./screens/Profile/Profile.jsx";
 import Register from "./screens/Registration/Register.jsx";
 import PaymentSuccess from "./screens/Checkout/PaymentSuccess.jsx";
 import {logout} from "./utils/auth.js";
+import ResetPassword from "./screens/Authentication/ResetPassword.jsx";
 
 function App() {
     const [user, setUser] = useState(null);
@@ -39,6 +40,7 @@ function App() {
       <Routes>
           <Route index path="/:contact?" element={user ? <Profile/> : <Homepage/>}/>
           <Route path="/login" element={user ? <Navigate to="/profile"/> : <Login user={user}/>}/>
+          <Route path="/reset" element={user ? <Navigate to="/profile"/> : <ResetPassword/>}/>
           <Route path="/referral/:referral" element={<ReferralLink/>}/>
           <Route path="/support" element={<Support/>}/>
           <Route path="/profile" element={user ? <Profile/> : <Navigate to="/login"/>}/>
