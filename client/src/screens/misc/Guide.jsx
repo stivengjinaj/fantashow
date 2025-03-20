7621dbb1c42af43818090bf4906828808ae361c1
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-03-20 22:05:55 +0100

    Made a component out of guide button

---
+++ client/src/screens/misc/Guide.jsx
@@ -0,0 +1,14 @@
+import {Button, Container} from "react-bootstrap";
+
+
+function Guide() {
+    return (
+        <Container fluid className="d-flex align-items-center justify-content-end">
+            <Button className="guideButton px-5 mt-4 mx-3 fw-bold">
+                Guida
+            </Button>
+        </Container>
+    );
+}
+
+export default Guide;
