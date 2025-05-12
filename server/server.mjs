e9d366adae319b30e3352f2d723b5f678559d5c2
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-05-12 10:09:37 +0200

    Set trust proxy to 1 for improved security configuration

--- server/server.mjs
+++ server/server.mjs
@@ -16,7 +16,7 @@ const corsOptions = {
     credentials: true,
 };
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
