ce8c2e6c65b1d30695e368386ce0addbef22c132
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-05-18 17:20:38 +0200

    cleanup

--- server/generalRoutes.mjs
+++ server/generalRoutes.mjs
@@ -9,8 +9,8 @@ dotenv.config();
 const generalRoutes = express.Router();
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000,
+    max: 100,
     message: 'Too many requests from this IP, please try again later.',
 });
 
