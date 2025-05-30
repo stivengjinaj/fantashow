d0a9450fd109189e0249518a6be8432c02dd2c94
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-05-30 22:52:13 +0200

    Update Stripe API key to use live environment

--- client/src/utils/stripe.js
+++ client/src/utils/stripe.js
@@ -1,6 +1,6 @@
 import {loadStripe} from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe("pk_test_51R1TZaFA0oBwMjT9N47Y7sXK0hWYE5TsDQuwBsiOCqLaRhHVsWxVDqMAe4x8xHzaqiAsXaX6fAr7o7VXFsSbKzST00HtyK8yOm", {
+export const stripePromise = loadStripe("pk_live_51R6E7vJxIWLRjTTs3zjLiTN0R53mWgns5gVyMEpnuYbR8fHhoPaNKGc8Gz1lq4fd96Ox2rqLJ7jBonPTlZV2Y3AZ00unGWvKxK", {
     betas: ['custom_checkout_beta_5'],
 });
 
